d2bb5031cee9b5225e9652915df9cf657bd47d58
Vincent Paradis <paradis.vincent@hotmail.ca>
2018-11-14 23:10:12 -0500

    Changes for a decent publish on npm

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,3 @@
-import Vue from 'vue'
 import RadialMenu from './RadialMenu.vue'
-import RadialMenuItem from './RadialMenuItem'
 
-const Components = {
-    RadialMenu,
-    RadialMenuItem
-}
-
-Object.keys(Components).forEach(c => {
-    Vue.component(c, Components[c])
-})
-
-export default Components
+export default RadialMenu

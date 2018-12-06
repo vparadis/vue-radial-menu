9b573fa4ba2c410578cd3aafef8d5d5677cfeb47
Vincent Paradis <paradis.vincent@hotmail.ca>
2018-12-06 12:07:56 -0500

    Removed tag filter as it can differ

--- src/components/RadialMenu.vue
+++ src/components/RadialMenu.vue
@@ -10,108 +10,111 @@
 
 <style>
 .vue-radial-menu-wrapper {
-    position: relative;
-    user-select: none;
-    border-radius: 50%;
-    font-size: 32px;
-    font-weight: bold;
-    border-radius: 50%;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.24), 0 0 0 rgba(0,0,0,0.16);
+	position: relative;
+	user-select: none;
+	border-radius: 50%;
+	font-size: 32px;
+	font-weight: bold;
+	border-radius: 50%;
+	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.24), 0 0 0 rgba(0, 0, 0, 0.16);
 }
 
 .vue-radial-menu-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    transition: all 0.2s ease;
-    position: relative;
-    z-index: 6;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 50%;
+	transition: all 0.2s ease;
+	position: relative;
+	z-index: 6;
 }
 
 .vue-radial-menu-container.open {
-    transform: rotateZ(45deg);
+	transform: rotateZ(45deg);
 }
 </style>
 
 <script>
 const RadialMenu = {
-    name: 'RadialMenu',
-    props: {
-        angleRestriction: { type: Number, default: 180 },
-        size: { type: Number, default: 50 },
-        itemSize: { type: Number, default: 36 },
-        rotate: { type: Number, default: 0 },
-        radius: { type: Number, default: 100 },
-        open: { type: Boolean, default: undefined }
+	name: 'RadialMenu',
+	props: {
+		angleRestriction: { type: Number, default: 180 },
+		size: { type: Number, default: 50 },
+		itemSize: { type: Number, default: 36 },
+		rotate: { type: Number, default: 0 },
+		radius: { type: Number, default: 100 },
+		open: { type: Boolean, default: undefined }
+	},
+	data() {
+		const { size } = this;
+		const manualMode = typeof this.open !== 'undefined';
+		return {
+			manualMode,
+			isOpen: false,
+			style: {
+				width: size + 'px',
+				height: size + 'px'
+			}
+		};
+	},
+	computed: {
+		shouldOpen() {
+			const { open, manualMode, isOpen } = this;
+			return manualMode ? open : isOpen;
+		}
+	},
+	mounted() {
+		document.addEventListener('click', this.closeMenuEvent);
+		this.setChildProps();
+	},
+	beforeUpdate() {
+		this.setChildProps();
+	},
+	beforeDestroy() {
+		document.removeEventListener('click', this.closeMenuEvent);
+	},
+	methods: {
+		closeMenuEvent(e) {
+			if (this.shouldOpen && !this.$el.contains(e.target)) {
+				this.toggleMenu();
+			}
+		},
+		handleClick() {
+			this.$emit('click');
+			this.toggleMenu();
+		},
+		toggleMenu() {
+			if (this.manualMode) return;
 
-    },
-    data () {
-        const { size } = this;
-        const manualMode = typeof(this.open) !== 'undefined'
-        return {
-            manualMode,
-            isOpen: false,
-            style: {
-                width: size + 'px',
-                height: size + 'px'
-            }
-        };
-    },
-    computed: {
-        shouldOpen () {
-            const { open, manualMode, isOpen } = this
-            return manualMode ? open : isOpen
-        }
-    },
-    mounted () {
-        document.addEventListener('click', this.closeMenuEvent);
-        this.setChildProps();
-    },
-    beforeUpdate () {
-        this.setChildProps();
-    },
-    beforeDestroy () {
-        document.removeEventListener('click', this.closeMenuEvent);
-    },
-    methods: {
-        closeMenuEvent (e) {
-            if (this.shouldOpen && !this.$el.contains(e.target)) { this.toggleMenu(); }
-        },
-        handleClick () {
-            this.$emit('click')
-            this.toggleMenu()
-        },
-        toggleMenu () {
-            if (this.manualMode) return
+			if (this.isOpen) {
+				this.isOpen = false;
+				this.$emit('close');
+			} else {
+				this.isOpen = true;
+				this.$emit('open');
+			}
+		},
+		setChildProps() {
+			// Manually add prop data to the items
+			const items = this.$slots.default.map(s => s.componentOptions.propsData);
+			const { size, itemSize, angleRestriction, rotate, radius } = this;
+			const angle =
+				angleRestriction > 300 || angleRestriction < -300 ? 300 : angleRestriction;
+			const frags = angle / (items.length - 1 || 1);
+			const angles = items.map(
+				(item, index) => (Math.PI * (frags * index + rotate)) / 180
+			);
 
-            if (this.isOpen) {
-                this.isOpen = false
-                this.$emit('close')
-            } else {
-                this.isOpen = true
-                this.$emit('open')
-            }
-        },
-        setChildProps () {
-            // Manually add prop data to the items
-            const items = this.$slots.default
-                .filter((s) => s.componentOptions && s.componentOptions.tag === 'radial-menu-item') // Filter to only take the items that matter
-                .map((s) => s.componentOptions.propsData);
-            const { size, itemSize, angleRestriction, rotate, radius } = this;
-            const angle = angleRestriction > 300 || angleRestriction < -300 ? 300 : angleRestriction;
-            const frags = angle / (items.length - 1 || 1);
-            const angles = items.map((item, index) => ((Math.PI * (frags * index + rotate)) / 180));
-
-            items.forEach((propData, index) => {
-                propData.width = itemSize;
-                propData.height = itemSize;
-                propData.left = -1 * ((size / 2) + Math.cos(angles[index]) * radius - (itemSize / 2)); // -1 to have the items in the right order
-                propData.top = (size / 2) - Math.sin(angles[index]) * radius - (itemSize / 2);
-                propData.onClick = this.manualMode ? null : this.toggleMenu; // To prevent double emiting click event
-            });
-        }
-    }
+			items.forEach((propData, index) => {
+				propData.width = itemSize;
+				propData.height = itemSize;
+				propData.left =
+					-1 * (size / 2 + Math.cos(angles[index]) * radius - itemSize / 2); // -1 to have the items in the right order
+				propData.top = size / 2 - Math.sin(angles[index]) * radius - itemSize / 2;
+				propData.onClick = this.manualMode ? null : this.toggleMenu; // To prevent double emiting click event
+			});
+		}
+	}
 };
 
 export default RadialMenu;
--- src/components/RadialMenuItem.vue
+++ src/components/RadialMenuItem.vue
@@ -1,60 +1,60 @@
 <template>
-    <transition name="radial-vue-radial-menu-item">
+    <Transition name="radial-vue-radial-menu-item">
         <div
             :style="style"
             class="vue-radial-menu-item"
             @click="handleClick">
             <slot></slot>
         </div>
-    </transition>
+    </Transition>
 </template>
 
 <style>
 .vue-radial-menu-item {
-    position: absolute;
-    border-radius: 50%;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: all 0.2s ease;
-    z-index: 5;
-    transform: none;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.24), 0 0 0 rgba(0,0,0,0.16);
-    user-select: none;
-    font-size: 16px;
+	position: absolute;
+	border-radius: 50%;
+	box-sizing: border-box;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	transition: all 0.2s ease;
+	z-index: 5;
+	transform: none;
+	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.24), 0 0 0 rgba(0, 0, 0, 0.16);
+	user-select: none;
+	font-size: 16px;
 }
 
 .radial-vue-radial-menu-item-enter,
 .radial-vue-radial-menu-item-leave-to {
-  top: 50% !important;
-  left: 50% !important;
-  transform: translate(-50%, -50%);
-  opacity: 0;
+	top: 50% !important;
+	left: 50% !important;
+	transform: translate(-50%, -50%);
+	opacity: 0;
 }
 </style>
 
 <script>
 const RadialMenuItem = {
-    name: 'RadialMenuItem',
-    props: ['top', 'left', 'height', 'width', 'onClick'],
-    data () {
-        const { top, left, height, width } = this;
-        return {
-            style: {
-                top: top + 'px',
-                left: left + 'px',
-                width: width + 'px',
-                height: height + 'px'
-            }
-        };
-    },
-    methods: {
-        handleClick (e) {
-            this.onClick && this.onClick(e);
-            this.$emit('click', e);
-        }
-    }
+	name: 'RadialMenuItem',
+	props: ['top', 'left', 'height', 'width', 'onClick'],
+	data() {
+		const { top, left, height, width } = this;
+		return {
+			style: {
+				top: top + 'px',
+				left: left + 'px',
+				width: width + 'px',
+				height: height + 'px'
+			}
+		};
+	},
+	methods: {
+		handleClick(e) {
+			this.onClick && this.onClick(e);
+			this.$emit('click', e);
+		}
+	}
 };
 
 export default RadialMenuItem;
